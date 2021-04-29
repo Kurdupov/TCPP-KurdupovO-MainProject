f5fc61cb7bcb6463b4e70e56e1f84c4e3d29ae14
suxanovvv <suxanov.2001.andrey@gmail.com>
2021-04-29 10:40:33 +0300

    Зміни_2

--- HelloWORLD1/Program.cs
+++ HelloWORLD1/Program.cs
@@ -10,8 +10,7 @@ namespace HelloWORLD1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Привіт, Донну!");
             Console.WriteLine("Привіт, Донну та світ!");
-            Console.WriteLine("Привіт, Донну!!!!");
-            Console.WriteLine();
+            Console.WriteLine("Привіт, Донну!!!");
         }
     }
 }
